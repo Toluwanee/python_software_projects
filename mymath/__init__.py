9ff4993fdc663c88cafb328c6a6c467145827a92
Toluwanee <toluwalope9@gmail.com>
2024-11-20 13:04:51 +0100

    mymath

---
+++ mymath/__init__.py
@@ -0,0 +1,5 @@
+#this demonstrates how to create package in  python
+
+from . import basic
+from . import stats
+from . import geometry
---
+++ mymath/geometry.py
@@ -0,0 +1,11 @@
+def area_of_rectangle(l, b):
+    """
+    This function returns the area of a rectangle
+    """
+    return l * b
+  
+def area_of_circle(r):
+    """
+    This function returns the area of a circle
+    """
+    return 3.142 * r
